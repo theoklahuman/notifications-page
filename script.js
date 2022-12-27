fce46b85bc7762c2e483c6557ac5ae698fcb5a2f
theoklahuman <theoklahuman@gmail.com>
2022-12-27 11:31:19 -0600

    Fix notification count

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const notificationContainerUnread = document.querySelectorAll(
   ".notification-container__unread"
 );
 const span = document.querySelectorAll("span");
-
+const notificationCount = document.querySelector(".notification-count");
 const markRead = document.querySelector(".mark-read");
 
+console.log(notificationCount.textContent);
+
 function markAsRead() {
+    notificationCount.textContent = "0";
   for (item of notificationContainerUnread) {
     item.className = "notification-container__read";
   }
